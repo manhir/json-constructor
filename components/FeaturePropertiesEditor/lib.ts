9e4cbc63ca97404681b6493bbf342faaf8cf86a4
manhir <53478716+manhir@users.noreply.github.com>
2020-06-23 18:14:38 +0300

    deletet old form view

--- components/FeaturePropertiesEditor/lib.ts
+++
@@ -1,6 +0,0 @@
-export function changeItem<T>(items: T[], index: number, partialValue: Partial<T>): T[] {
-    return items.map((item, i) => i !== index ? item : ({
-        ...item,
-        ...partialValue,
-    }))
-}
